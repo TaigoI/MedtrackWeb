c0768b89d74f8088f0d70cfd5e502c79f15cd0fd
assisnat <nas@ic.ufal.br>
2023-05-24 22:14:23 -0300

    Altera as páginas de login e signup

--- src/presentation/pages/auth/login.tsx
+++ src/presentation/pages/auth/login.tsx
@@ -15,7 +15,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { literal, object, string, TypeOf } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FormInput } from '../../components/auth/form-input';
-
+import MedicalImage from '../../../assets/images/consulta_medica.jpg'
 import styled from '@emotion/styled';
 import { useAuthentication } from '../../context/AuthenticationContext';
 import { Cpf } from '../../../modules/authentication/entities/Cpf';
@@ -30,13 +30,12 @@ export const LinkItem = styled(Link)`
   }
 `;
 
-
 const cpfSchema = string().refine((value) => /^\d{11}$/.test(value.replace(/\D+/, '')), {
-    message: 'Invalid CPF format',
-  }).transform((value) => {
-    const maskedCPF = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
-    return maskedCPF;
-  })
+  message: 'Invalid CPF format',
+}).transform((value) => {
+  const maskedCPF = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  return maskedCPF;
+})
 const loginSchema = object({
   cpf: string(),
   password: string()
@@ -72,186 +71,164 @@ export const LoginPage: FC = () => {
 
       toast.success(`Bem vindo, ${_user.name}!`, {
         theme: 'colored'
-      }); 
+      });
     } catch (error) {
       toast.error('Dados inválidos! Tente novamente!', {
         theme: 'colored'
-      }); 
+      });
     }
   };
 
   useEffect(() => {
 
     const cpfInput = document.getElementById('cpfInput');
-  
+
     cpfInput?.addEventListener('input', (event: any) => {
       const input = event.target;
       let value = input?.value.replace(/\D/g, ''); // Remove non-digit characters
-  
+
       if (value.length > 11) {
         value = value.slice(0, 11); // Limit to 11 digits
       }
-  
+
       const maskedValue = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
       methods.setValue('cpf', maskedValue)
     });
   }, []);
 
-
-
   return (
-    <Container
-      maxWidth={false}
-      sx={{ height: '100vh', backgroundColor: { xs: '#fff', md: '#3683dc' } }}
+    <Box
+      sx={{
+        display: "flex",
+        background: "#ddd",
+        minHeight: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <Grid
-        container
-        justifyContent='center'
-        alignItems='center'
-        sx={{ width: '100%', height: '100%' }}
+      <Box
+        sx={{
+          display: { sm: "none", md: "block" },
+          backgroundImage: `url(${MedicalImage})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          width: "75rem",
+          height: "100vh",
+          boxShadow: {
+            sm: "0 0 5px #ddd",
+          },
+        }}
+      ></Box>
+      <Box
+        sx={{
+          background: "white",
+          width: "40rem",
+          padding: "5rem",
+          boxShadow: {
+            sm: "0 0 5px #ddd",
+          },
+          alignSelf: { sm: "flex-end", md: "stretch" },
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <Grid
-          item
-          sx={{ maxWidth: '70rem', width: '100%', backgroundColor: '#fff' }}
-        >
-          <FormProvider {...methods}>
-            <Grid
-              container
-              sx={{
-                boxShadow: { sm: '0 0 5px #ddd' },
-                py: '6rem',
-                px: '1rem',
-              }}
-            >
-              <Grid
-                item
-                container
-                justifyContent='space-between'
-                rowSpacing={5}
-                sx={{
-                  maxWidth: { sm: '45rem' },
-                  marginInline: 'auto',
-                }}
-              >
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                  sx={{ borderRight: { sm: '1px solid #ddd' } }}
-                >
-                  <Box
-                    display='flex'
-                    flexDirection='column'
-                    component='form'
-                    noValidate
-                    autoComplete='off'
-                    sx={{ paddingRight: { sm: '3rem' } }}
+        <FormProvider {...methods}>
+          <Typography
+            variant="h6"
+            component="h1"
+            sx={{ textAlign: 'center', mb: '1.5rem' }}
+          >
+            Acesse sua conta
+          </Typography>
+
+          <Controller
+            control={methods.control}
+            name="cpf"
+            render={({ field: { onChange, onBlur, value, ref } }) => (
+              <FormInput
+                label="CPF"
+                type="text"
+                name="cpf"
+                id="cpfInput"
+                focused
+                required
+                onChange={onChange}
+                value={value}
+                sx={{ width: "400px" }}
+              />
+            )}
+          />
+          <Controller
+            control={methods.control}
+            name="password"
+            render={({ field: { onChange, onBlur, value, ref } }) => (
+              <FormInput
+                label="Senha"
+                onChange={onChange}
+                value={value}
+                type="password"
+                name="password"
+                required
+                focused
+                sx={{ width: "400px" }}
+              />
+            )}
+          />
+
+          <Controller
+            control={methods.control}
+            name="persistUser"
+            render={({ field: { onChange, onBlur, value, ref } }) => (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    size="small"
+                    aria-label="trust this device checkbox"
+                    required
+                    name="persistUser"
+                    onChange={onChange}
+                    value={value}
+                  />
+                }
+                label={
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      fontSize: '0.8rem',
+                      fontWeight: 400,
+                      color: '#5e5b5d',
+                    }}
                   >
-                    <Typography
-                      variant='h6'
-                      component='h1'
-                      sx={{ textAlign: 'center', mb: '1.5rem' }}
-                    >
-                      Acesse sua conta
-                    </Typography>
-
-                    <Controller
-                      control={methods.control}
-                      name='cpf'
-                      render={({ field: { onChange, onBlur, value, ref } }) => (
-                        <FormInput
-                          label='CPF'
-                          type='text'
-                          name='cpf'
-                          id="cpfInput"
-                          focused
-                          required
-                          onChange={onChange}
-                          value={value}
-                        />
-                      )}
-                    />
-                    <Controller
-                      control={methods.control}
-                      name='password'
-                      render={({ field: { onChange, onBlur, value, ref } }) => (
-                        <FormInput
-                          label='Senha'
-                          onChange={onChange}
-                          value={value}
-                          type='password'
-                          name='password'
-                          required
-                          focused
-                        />
-                      )}
-                    />
-                    
-                    <Controller
-                      control={methods.control}
-                      name='persistUser'
-                      render={({ field: { onChange, onBlur, value, ref } }) => (
-                          <FormControlLabel
-                            control={
-                              <Checkbox
-                                size='small'
-                                aria-label='trust this device checkbox'
-                                required
-                                name='persistUser'
-                                onChange={onChange}
-                                value={value}
-                              />
-                            }
-                            label={
-                              <Typography
-                                variant='body2'
-                                sx={{
-                                  fontSize: '0.8rem',
-                                  fontWeight: 400,
-                                  color: '#5e5b5d',
-                                }}
-                              >
-                                Mantenha-me conectado.
-                              </Typography>
-                            }
-                          />
-                      )}
-                    />
-                    
-
-                    <LoadingButton
-                      loading={false}
-                      type='submit'
-                      variant='contained'
-                      onClick={methods.handleSubmit(onSubmitHandler)}
-                      sx={{
-                        py: '0.8rem',
-                        mt: 2,
-                        width: '80%',
-                        marginInline: 'auto',
-                      }}
-                    >
-                      Login
-                    </LoadingButton>
-                  </Box>
-                </Grid>
-              </Grid>
-              <Grid container justifyContent='center'>
-                <Stack sx={{ mt: '3rem', textAlign: 'center' }}>
-                  <Typography sx={{ fontSize: '0.9rem', mb: '1rem' }}>
-                    Não possui conta?{' '}
-                    <LinkItem to='/signup'>Cadastre-se</LinkItem>
-                    .
+                    Mantenha-me conectado.
                   </Typography>
-                  <Typography sx={{ fontSize: '0.9rem' }}>
-                    <LinkItem to='/forgotPassword'>Esqueceu sua senha?</LinkItem>
-                  </Typography>
-                </Stack>
-              </Grid>
-            </Grid>
-          </FormProvider>
-        </Grid>
-      </Grid>
-    </Container>
+                }
+              />
+            )}
+          />
+
+          <LoadingButton
+            loading={false}
+            type="submit"
+            variant="contained"
+            onClick={methods.handleSubmit(onSubmitHandler)}
+            sx={{
+              py: '0.8rem',
+              mt: 2,
+              width: '60%',
+              marginInline: 'auto',
+            }}
+          >
+            Login
+          </LoadingButton>
+          <Stack sx={{ mt: '3rem', textAlign: 'center' }}>
+            <Typography sx={{ fontSize: '0.9rem', mb: '1rem' }}>
+              Não possui conta? <LinkItem to="/signup">Cadastre-se</LinkItem>.
+            </Typography>
+          </Stack>
+        </FormProvider>
+      </Box>
+    </Box>
   );
 };
--- src/presentation/pages/auth/signup.tsx
+++ src/presentation/pages/auth/signup.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography, Stack } from "@mui/material";
+import { Box, Typography, Stack } from "@mui/material";
 import { FC, useEffect } from "react";
 import {
   useForm,
@@ -29,7 +29,7 @@ const signupSchema = object({
   mobilePhone: string().regex(/^\d{11}$/, 'O número inserido é inválido.').max(11),
   businessPhone: string().regex(/^\d{10}$/, 'O número inserido é inválido.').max(10),
   //gender: string().regex(),
-  email: string().min(1, "Digite um email.").email("O email é inválido."), 
+  email: string().min(1, "Digite um email.").email("O email é inválido."),
   password: string()
     .min(1, "Digite uma senha.")
     .min(8, "A senha precisa ter mais que 8 caracteres.")
@@ -54,7 +54,6 @@ const SignupForm: FC<{
         noValidate
         autoComplete="off"
         sx={{ width: "80%" }}
-        // onSubmit={methods.handleSubmit(onSubmit)}
       >
         <Typography
           variant="h6"
@@ -73,7 +72,7 @@ const SignupForm: FC<{
               type="text"
               name="name"
               focused
-              required 
+              required
               onChange={onChange}
               value={value}
             />
@@ -84,99 +83,99 @@ const SignupForm: FC<{
           name="cpf"
           control={methods.control}
           render={({ field: { onChange, onBlur, value, ref } }) => (
-            <FormInput 
-              label="CPF" 
-              type="text" 
+            <FormInput
+              label="CPF"
+              type="text"
               id="cpfInput"
-              name="cpf" 
+              name="cpf"
               placeholder="111.222.333-11"
               focused
               required
               onChange={onChange}
               value={value}
               sx={{ w: "100%" }} />
-          )} 
+          )}
         />
         <Controller
           name="crm"
           control={methods.control}
           render={({ field: { onChange, onBlur, value, ref } }) => (
-            <FormInput 
-              label="CRM" 
-              type="text" 
-              name="crm" 
-              placeholder="CRM-AL 1234" focused required 
+            <FormInput
+              label="CRM"
+              type="text"
+              name="crm"
+              placeholder="CRM-AL 1234" focused required
               onChange={onChange}
               value={value}
-              sx={{ w: "100%" }} 
+              sx={{ w: "100%" }}
             />
-          )} 
+          )}
         />
         <Controller
           name="birth"
           control={methods.control}
           render={({ field: { onChange, onBlur, value, ref } }) => (
-            <FormInput 
-              label="Data de nascimento" 
-              type="text" 
-              name="birth" 
+            <FormInput
+              label="Data de nascimento"
+              type="text"
+              name="birth"
               onChange={onChange}
               value={value}
-              placeholder="XX/XX/XXXX" focused required 
+              placeholder="XX/XX/XXXX" focused required
             />
-          )} 
+          )}
         />
         <Controller
           name="mobilePhone"
           control={methods.control}
           render={({ field: { onChange, onBlur, value, ref } }) => (
-            <FormInput 
-              label="Contato pessoal" 
-              type="text" 
-              name="mobilePhone" 
+            <FormInput
+              label="Contato pessoal"
+              type="text"
+              name="mobilePhone"
               onChange={onChange}
               value={value}
               placeholder="Ex: 82990000000" focused required />
-          )} 
+          )}
         />
         <Controller
           name="businessPhone"
           control={methods.control}
           render={({ field: { onChange, onBlur, value, ref } }) => (
-            <FormInput 
-              label="Contato profissional" 
-              type="text" 
-              name="businessPhone" 
+            <FormInput
+              label="Contato profissional"
+              type="text"
+              name="businessPhone"
               onChange={onChange}
               value={value}
-              placeholder="Ex: 8299000000" focused required 
+              placeholder="Ex: 8299000000" focused required
             />
-          )} 
+          )}
         />
         <Controller
           name="email"
           control={methods.control}
           render={({ field: { onChange, onBlur, value, ref } }) => (
-            <FormInput 
-              label="Email" 
-              type="email" 
-              name="email" 
+            <FormInput
+              label="Email"
+              type="email"
+              name="email"
               onChange={onChange}
               value={value}
               placeholder="email@email.com" focused required />
-          )} 
+          )}
         />
         <Controller
           name="password"
           control={methods.control}
           render={({ field: { onChange, onBlur, value, ref } }) => (
-            <FormInput 
-              label="Senha" 
-              type="password" 
-              name="password" 
+            <FormInput
+              label="Senha"
+              type="password"
+              name="password"
               onChange={onChange}
               value={value}
-              placeholder="" focused required 
+              placeholder="" focused required
             />
           )}
         />
@@ -184,10 +183,10 @@ const SignupForm: FC<{
           name="passwordConfirm"
           control={methods.control}
           render={({ field: { onChange, onBlur, value, ref } }) => (
-            <FormInput 
-              label="Confirme a senha" 
-              type="password" 
-              name="passwordConfirm" required focused 
+            <FormInput
+              label="Confirme a senha"
+              type="password"
+              name="passwordConfirm" required focused
               onChange={onChange}
               value={value}
             />
@@ -264,22 +263,22 @@ export const SignupPage: FC = () => {
     } catch (error) {
       toast.error('Erro ao cadastrar', {
         theme: 'colored'
-      }); 
+      });
     }
   };
 
   useEffect(() => {
 
     const cpfInput = document.getElementById('cpfInput');
-  
+
     cpfInput?.addEventListener('input', (event: any) => {
       const input = event.target;
       let value = input?.value.replace(/\D/g, ''); // Remove non-digit characters
-  
+
       if (value.length > 11) {
         value = value.slice(0, 11); // Limit to 11 digits
       }
-  
+
       const maskedValue = value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
       methods.setValue('cpf', maskedValue)
     });
@@ -303,7 +302,7 @@ export const SignupPage: FC = () => {
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
           width: "75rem",
-          height: "150vh",
+          height: "160vh",
           boxShadow: {
             sm: "0 0 5px #ddd",
           },
@@ -320,8 +319,8 @@ export const SignupPage: FC = () => {
           alignSelf: { sm: "flex-end", md: "stretch" },
           display: "flex",
           flexDirection: "column",
-          justifyContent: "center", 
-          alignItems: "center", 
+          justifyContent: "center",
+          alignItems: "center",
         }}
       >
         <SignupForm methods={methods} onSubmit={onSubmitHandler} />
